33e94693fa7607a0fc3eb4104cce7211f438a4b5
elrojojo <christoph.kuepferle@web.de>
2022-09-29 19:33:25 +0200

    composer create-project codeigniter4/appstarter project-root

--- application/views/login/logout.php
+++
@@ -1,5 +0,0 @@
-<script type='text/javascript'>
-  localStorage.removeItem( cookie_redundanz_name('vereinsapp_dauerhaft_angemeldet_identifier') );
-  localStorage.removeItem( cookie_redundanz_name('vereinsapp_dauerhaft_angemeldet_securitytoken') );
-</script>
-
--- application/views/templates/status.php
+++
@@ -1,6 +0,0 @@
-<div class="col-md-12"><div class="alert alert-<?php echo $farbe; ?><?php if( isset($dismissible) AND $dismissible ) echo ' alert-dismissible fade show'; ?>" role="alert">
-  <?php print_r( $status ); ?>
-  <?php if( isset($dismissible) AND $dismissible ) { ?><button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button><?php } ?>
-</div></div>
