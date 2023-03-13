81a897b74c613ad0a48a4cc6f3ff4db6a607aee9
elrojojo <christoph.kuepferle@web.de>
2023-03-13 14:29:58 +0100

    vereinsapp basis

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# vereinsapp
- 
+# Vereinsapp Basis (Codeigniter 4 & Shield)
--- application/views/templates/liste_fuss.php
+++
@@ -1,6 +0,0 @@
-</ul><?php if( !array_key_exists( 'beschriftung_h5', $liste ) ) { ?></div><?php } ?>
-<?php if( array_key_exists( 'form_inputs', $liste ) ) foreach( $liste['form_inputs'] as $name => $value ): ?><input type="text" class="invisible" name="<?php echo $name; ?>" value="<?php echo $value; ?>" /><?php endforeach; ?>
-<?php if( array_key_exists( 'fuss_werkzeugkasten', $liste) ) echo $liste['fuss_werkzeugkasten']; ?>
-<?php if( array_key_exists( 'form', $liste ) ) echo '</form>' ?>
-
-
