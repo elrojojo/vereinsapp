a3ca927cda9b9954eaa7fa3f4dacca6ad13e2a28
vereinsapp <93478804+vereinsapp@users.noreply.github.com>
2023-10-22 16:26:03 +0200

    0.1.9

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -78,7 +78,7 @@ abstract class BaseController extends Controller
         defined('HEAD_STYLESHEET') OR define( 'HEAD_STYLESHEET', array( 
           array( 'href' => 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css', 'integrity' => 'sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65', 'crossorigin' => 'anonymous', ),
           array( 'href' => 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css'),
-          array( 'href' => base_url('css/vereinsapp/vereinsapp.css'), ),
+          array( 'href' => base_url('css/vereinsapp.css'), ),
         ) );
         $head_script = array( 
           array( 'src' => 'https://code.jquery.com/jquery-3.6.3.js', 'integrity' => 'sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=', 'crossorigin' => 'anonymous', ),
