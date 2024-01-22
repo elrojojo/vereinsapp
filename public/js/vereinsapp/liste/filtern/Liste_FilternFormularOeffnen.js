6186e9a1b4fe007c5dd2fa4f11b4f9fc96d862ae
vereinsapp <93478804+vereinsapp@users.noreply.github.com>
2024-01-22 22:23:22 +0100

    0.9.1.34.1.17.1.5
    filtern_liste eingeführt

--- public/js/vereinsapp/liste/filtern/Liste_FilternFormularOeffnen.js
+++ public/js/vereinsapp/liste/filtern/Liste_FilternFormularOeffnen.js
@@ -2,17 +2,20 @@ function Liste_FilternFormularOeffnen($formular, $btn_oeffnend) {
     const liste = $btn_oeffnend.attr("data-liste");
     const instanz = $btn_oeffnend.attr("data-instanz");
     const eigenschaft = $btn_oeffnend.attr("data-eigenschaft");
+    let filtern_liste = $btn_oeffnend.attr("data-filtern_liste");
     const element_id = $btn_oeffnend.attr("data-element_id");
 
     $formular.find(".filtern, .filtern_definitionen").attr("data-liste", liste);
     if (typeof instanz !== "undefined") $formular.find(".filtern, .filtern_definitionen").attr("data-instanz", instanz);
     if (typeof eigenschaft !== "undefined") $formular.find(".filtern, .filtern_definitionen").attr("data-eigenschaft", eigenschaft);
+    if (typeof filtern_liste !== "undefined") $formular.find(".filtern, .filtern_definitionen").attr("data-filtern_liste", filtern_liste);
+    else filtern_liste = liste;
     if (typeof element_id !== "undefined") $formular.find(".filtern, .filtern_definitionen").attr("data-element_id", element_id);
 
     $(".filtern_definitionen").empty();
-    $.each(FILTERBARE_EIGENSCHAFTEN[liste], function (index, eigenschaft_) {
-        const typ = EIGENSCHAFTEN[liste][eigenschaft_].typ;
-        const beschriftung = EIGENSCHAFTEN[liste][eigenschaft_].beschriftung;
+    $.each(FILTERBARE_EIGENSCHAFTEN[filtern_liste], function (index, eigenschaft_) {
+        const typ = EIGENSCHAFTEN[filtern_liste][eigenschaft_].typ;
+        const beschriftung = EIGENSCHAFTEN[filtern_liste][eigenschaft_].beschriftung;
 
         const $neue_filtern_definition = FILTERN.$blanko_filtern_definition[typ]
             .clone()
@@ -29,7 +32,7 @@ function Liste_FilternFormularOeffnen($formular, $btn_oeffnend) {
 
         if (typ == "vorgegebene_werte") {
             $neue_filtern_definition.find(".filtern_wert").empty();
-            $.each(VORGEGEBENE_WERTE[liste][eigenschaft_], function (wert, eigenschaften) {
+            $.each(VORGEGEBENE_WERTE[filtern_liste][eigenschaft_], function (wert, eigenschaften) {
                 $('<option value="' + wert + '">' + eigenschaften.beschriftung + "</option>").appendTo(
                     $neue_filtern_definition.find(".filtern_wert")
                 );
@@ -39,6 +42,8 @@ function Liste_FilternFormularOeffnen($formular, $btn_oeffnend) {
         $neue_filtern_definition.find(".btn_filtern_erstellen").first().attr("data-liste", liste);
         if (typeof instanz !== "undefined") $neue_filtern_definition.find(".btn_filtern_erstellen").first().attr("data-instanz", instanz);
         if (typeof eigenschaft !== "undefined") $neue_filtern_definition.find(".btn_filtern_erstellen").first().attr("data-eigenschaft", eigenschaft);
+        if (typeof filtern_liste !== "undefined")
+            $neue_filtern_definition.find(".btn_filtern_erstellen").first().attr("data-filtern_liste", filtern_liste);
         if (typeof element_id !== "undefined") $neue_filtern_definition.find(".btn_filtern_erstellen").first().attr("data-element_id", element_id);
 
         $neue_filtern_definition.appendTo($formular.find(".filtern_definitionen"));
--- public/js/vereinsapp/schnittstelle/event/Schnittstelle_EventVariableUpdDom.js
+++ public/js/vereinsapp/schnittstelle/event/Schnittstelle_EventVariableUpdDom.js
@@ -56,13 +56,14 @@ function Schnittstelle_EventVariableUpdDom(liste, naechste_aktionen) {
         $('.filtern[data-liste="' + liste + '"][data-eigenschaft^="filtern_"]').each(function () {
             const $filtern = $(this);
             const eigenschaft = $filtern.attr("data-eigenschaft");
+            const filtern_liste = $filtern.attr("data-filtern_liste");
             const element_id = $filtern.attr("data-element_id");
             $filtern.html(
                 Liste_Filtern2$FilternZurueck(
                     Schnittstelle_VariableRausZurueck(eigenschaft, element_id, liste),
                     FILTERN.$blanko_filtern_sammlung,
                     FILTERN.$blanko_filtern_element,
-                    liste
+                    filtern_liste
                 )
             );
             $filtern.find(".btn_filtern_aendern, .btn_filtern_loeschen").attr("data-liste", liste);
